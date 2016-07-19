d0176c86c49c2804630b254eaf1d2dd4a7f06314
vdigregorio <vdigregorio@Valerias-MacBook-Air.local>
2016-07-19 18:55:58 -0400

    fix #1

--- js/project.js
+++ js/project.js
@@ -2,14 +2,23 @@ var inputFtoC;
 var inputCtoF;
 
 function calculateTotal(){
-inputFtoC = document.getElementById("inputFtoC").value;
+
 inputCtoF = document.getElementById("inputCtoF").value;
 
-var solutionFtoC = (inputFtoC - 32) * (5 / 9);
 var solutionCtoF = inputCtoF * 9 / 5 + 32;
 
-document.getElementById("solutionFtoC").innerHTML = solutionFtoC;
+
 document.getElementById("solutionCtoF").innerHTML = solutionCtoF;
+}
+
+function calculateTotal(){
+inputFtoC = document.getElementById("inputFtoC").value;
+
+var solutionFtoC = (inputFtoC - 32) * (5 / 9);
+
+
+document.getElementById("solutionFtoC").innerHTML = solutionFtoC;
+
 }
 
 document.getElementById("convertFtoC").onclick = calculateTotal;
